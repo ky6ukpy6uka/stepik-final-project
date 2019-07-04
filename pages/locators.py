291a2bc2c0023612f1a85ae8ba4abf08816a7b81
ky6ukpy6uka <losvce@yandex.ru>
2019-07-04 10:01:44 +0300

    update locators for cart page

--- pages/locators.py
+++ pages/locators.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators(object):
-    LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
+	LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators(object):
     # добавить селекторы к формам регистрации и логина
@@ -11,12 +11,13 @@ class LoginPageLocators(object):
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
 class ProductPageLocators(object):
-    CART_LINK = (By.CSS_SELECTOR, ".btn-add-to-basket")
-    CART_PRODUCT_NAME = (By.CSS_SELECTOR, "#messages div:nth-child(1) div strong")
-    PAGE_PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
-    PAGE_PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
-    CART_PRODUCT_PRICE = (By.CSS_SELECTOR, ".alert-info div p:nth-child(1) strong")
+	ADD_TO_CART_LINK = (By.CSS_SELECTOR, ".btn-add-to-basket")
+	PAGE_PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+	PAGE_PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+	CART_PRODUCT_NAME = (By.CSS_SELECTOR, "#messages div:nth-child(1) div strong")
+	CART_PRODUCT_PRICE = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
 
 class BasePageLocators(object):
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+	LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+	LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+	CART_LINK = (By.CSS_SELECTOR, ".btn-group .btn-default:nth-child(1)")
