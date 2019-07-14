f8a1a24450adb6e60b15b84e52a89b33e0f307f9
Aleksey Belov <losvce@yandex.ru>
2019-07-14 14:20:17 +0300

    delete implicitly wait

--- pages/base_page.py
+++ pages/base_page.py
@@ -15,11 +15,14 @@ from .locators import BasePageLocators
 from selenium.common.exceptions import NoSuchElementException   #(имя исключения)
 
 class BasePage(object):
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(timeout)
+#        self.browser.implicitly_wait(10)
 
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                    " probably unauthorised user"
     def go_to_login_page(self):
         link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         link.click()
